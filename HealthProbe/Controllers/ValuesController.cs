3f68d862dcd9020b09f089d886c84cb089c82525
Razi Rais <razi@abmitt-lt.fareast.corp.microsoft.com>
2018-08-23 10:03:23 -0400

    Initial Commit

---
+++ HealthProbe/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthProbe.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthCheckController : Controller
+    {
+        static volatile int counter = 0;
+        [HttpGet]
+        public IActionResult Get()
+        {
+            counter++;
+            if (counter == 3)
+            {
+                return StatusCode(500);
+    
+            }
+            return StatusCode(200);
+        }
+    }
+}
